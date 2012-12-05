5424666ad20f4e656bc3593cf5abe802e00d7ade
Russell Toris <rctoris@wpi.edu>
2012-12-05 11:21:30 -0500

    index redirect pages changed to remove headers

--- www/js/rms/study.js
+++ www/js/rms/study.js
@@ -1,51 +1,21 @@
-/*********************************************************************
- *
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2012, Worcester Polytechnic Institute
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the Worcester Polytechnic Institute nor the 
- *     names of its contributors may be used to endorse or promote 
- *     products derived from this software without specific prior 
- *     written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- *   Author: Russell Toris
- *  Version: October 9, 2012
- *
- *********************************************************************/
+/**
+ * A collection of common Javascript functions for dealing with user studies.
+ * 
+ * @fileOverview A collection of common Javascript functions for dealing with user studies.
+ * @name RMS Study
+ * @author Russell Toris <rctoris@wpi.edu>
+ * @version December, 5 2012
+ */
 
 /**
- * Log the given message into the study_log MySQL table. This function 
- * will only work if the _EXPID global variable is defined.
+ * Log the given message into the study_log MySQL table. This function will only work if the _EXPID
+ * global variable is defined.
  * 
- * @param mesasge {string} the message to log
+ * @param {String}
+ *            message the message to log
  */
 function studyLog(message) {
-  if (typeof(_EXPID) !== 'undefined') {
+  if (typeof (_EXPID) !== 'undefined') {
     $.get('form/study_log.php?expid=' + _EXPID + '&entry=' + message);
   }
 }
